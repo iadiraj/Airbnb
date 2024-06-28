4b6f2d38d39de0fb3528fccf900d4c00c40603b7
ADITYA RAJ <developer.rajaditya@gmail.com>
2024-06-28 13:03:25 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by ADITYA RAJ on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
